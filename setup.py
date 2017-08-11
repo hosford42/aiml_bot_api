f209210f50be808b5c872404dde97dcfea8ec0c6
Aaron Hosford <aaron.hosford@ericsson.com>
2017-08-10 22:37:31 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,70 @@
+"""
+AIML Bot API
+============
+
+This is a very basic [GraphQL](http://graphql.org/) API for
+[AIML Bot](https://github.com/hosford42/aiml_bot).
+
+**IMPORTANT:** No security measures are implemented. Use this module
+as a public-facing API at your own risk. Anyone who has access to the
+API has access to the entire data set.
+
+Endpoints
+---------
+
+The following endpoints are provided:
+
+`/`
+
+The GraphQL endpoint is the preferred method for interacting with the system.
+
+`/users`
+
+A JSON endpoint for listing registered users or adding a new user.
+
+`/users/<user_id>`
+
+A JSON endpoint for retrieving information about a specific user.
+
+`/users/<user_id>/messages`
+
+A JSON endpoint for listing the messages to/from a user or sending
+a new message to the bot.
+
+`/users/<user_id>/messages/<message_id>`
+
+A JSON endpoint for retrieving information about a specific message.
+"""
 
 
 from setuptools import setup
 
 setup(
-    name='aiml_bot_api',
+    name='AIML Bot API',
     version='0.0',
-    modules=['aiml_bot_api'],
-    url='https://github.com/hosford42/aiml_bot_api',
-    license='MIT',
     author='Aaron Hosford',
     author_email='aaron.hosford@ericsson.com',
+    license='MIT',
     description='Json API to AIML Bot',
+    long_description=__doc__,
+    url='https://github.com/hosford42/aiml_bot_api',
+
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Communications :: Chat",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+
+    packages=['aiml_bot_api'],
     install_requires=[
         'flask',
         'flask_graphql',
         'graphene',
         'aiml_bot',
-    ]
+    ],
 )
